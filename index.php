1e0bbfab53336a0266db8c40e25de5271e019d75
zmaster <benja@zmaster.com.ar>
2013-10-05 11:11:41 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+  <title>Cómo donar</title>
+</head>
+<body>
+  <h1>Miniguía para donar</h1>
+ <p><h2>1.- Loguearse/Registrarse:</h2></p>
+ <img src="http://i.imgur.com/zCH8mBz.png">
+ 
+ <p><h2>2.- Cargar créditos en tu cuenta de donaciones:</h2></p>
+<p>Seleccionás <b>donar</b> y luego el medio de pago que prefieras (puede que aparezcan otros medios si sos de otro país).</p>
+ <img src="http://i.imgur.com/79Bazub.png">
+ 
+<p>Ejemplo Cuenta Digital:</p>
+<p>Ingresás el monto:</p>
+ <img src="http://i.imgur.com/NfPY9Qm.png">
+ 
+<p>Y te genera el ticket para pagar en un rapipago:</p>
+ <img src="http://i.imgur.com/Owaba7V.png">
+
+<p><h2>3.- Usar los créditos en el server que prefieras</h2></p>
+<p>Una vez que se acredite el dinero, te va a aparecer en el balance, vas al link <b>servicios</b>, elegís el server y en qué lo querés usar</p>
+ <img src="http://i.imgur.com/hWtIbjn.png">
+ 
+<p>Ahí mismo tenés la opción de convertir tu crédito en coins, por ejemplo para Ludmila:</p>
+ <img src="http://i.imgur.com/vqzcA1O.png">
+ 
+<p>Ponés en qué pj y cuántos créditos deseás convertir, y listo:</p>
+ <img src="http://i.imgur.com/ZIKsO52.png">
+
+</body>
+</html>
